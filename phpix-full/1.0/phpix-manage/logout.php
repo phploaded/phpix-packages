6f14eded57e07fc1c344c85bb20e91ad5ef196b6
phploaded <satish@phploaded.com>
2020-10-04 09:47:10 +0530

    stable package 1.0

--- phpix-full/1.0/phpix-manage/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-unset($_SESSION[$website_name]);
-
-?><script>
-localStorage.clear();
-document.location.href='<?php echo $admin_url ?>login';
-</script>
--- phpix-full/1.0/temp/ext/phpix-info.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$software_version = '1.1';
-$software_date = '1599748208';
-$software_updated = '1599748208';
-$software_jsonURL = 'http://phploaded.com/updates/updates.php';
-$software_zipURL = 'https://raw.githubusercontent.com/phploaded/PHPix/master/updates/';
-
- ?>
